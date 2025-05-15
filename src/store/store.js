ec7816770c8bd7f6637682d017f846824cac0eaa
shrnx <sharansahu1604@gmail.com>
2025-05-16 01:21:31 +0530

    very important naming convention in redux toolkit store

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,14 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice.js"
+// ye sirf import karte samay ka name hai kuch bhi likh sakte hai
+// As from authSlice.js we are exporting that reducer as default, so no worries. 
+// We can also import it like xyz, then use it like reducer: xyz
 
 const store = configureStore({
-    reducer: authReducer
+    reducer: {
+        authReducer
+    }
 });
 
 export default store;
