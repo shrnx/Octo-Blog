e2360ae9aba5ea405005d29fb17caf403f88c583
shrnx <sharansahu1604@gmail.com>
2025-05-16 00:56:20 +0530

    add Appwrite service for post and file management

---
+++ src/appwrite/config.js
@@ -0,0 +1,136 @@
+import conf from "../conf/conf.js"
+
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteURL)
+            .setProject(conf.appwriteProjectId);
+
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,   // Document ka id mang rahe hai yaha pe to usko id.unique bhi kar sakte hai but slug hi le liya 
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: createPost :: error", error);
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,   // slug ko humne document id banaya tha wahi yaha put karrahe 
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+
+            return true
+        } catch (error) {
+            console.log("Appwrite Service :: deletePost :: error", error);
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: getPost :: error", error);
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {  // Returns all posts whose status is active
+        try {
+            return await this.databases.listDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,       // Jo params me likhe hai wahi same yaha likh sakte the directly
+                //100 // Pagination
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    // File Upload
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: uploadFile :: error", error);
+            return false
+        }
+    }
+
+    // Delete File
+    async deleteFile() {
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId        // jab file create hua to return file Id hi hua hai usko hi save kia hai humne DB me
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    async getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service()
+export default service
