c2b33c3ee781815614f024e3182141e9836de873
shrnx <sharansahu1604@gmail.com>
2025-05-16 12:04:42 +0530

    implement user authentication in App component and add Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth.js"
+import { login, logout } from "./features/authSlice.js"
+import { set } from 'react-hook-form'
 
 function App() {
 
-  return (
-    <>
-      <h1>A FullStack Blog Application</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())    // Isse humesha humara state updated hi rahewga no matter logged in/out
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return loading
+    ? null
+    : <>
+          <div></div>
+      </>
 }
 
 export default App
