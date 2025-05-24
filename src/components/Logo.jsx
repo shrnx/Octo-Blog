67f6f351eb50911a90f629f85ba6b89b4c37bff7
shrnx <sharansahu1604@gmail.com>
2025-05-24 16:14:51 +0530

    feat: add Logo component and update index export

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = "100px"}) {  // This is optional property, if not given later this will be used.
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "../components/container/Container.jsx";
+import Logo from "./Logo.jsx";
+import LogoutButton from "./Header/LogoutButton.jsx";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutButton
 }
